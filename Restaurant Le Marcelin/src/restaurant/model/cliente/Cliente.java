a5756a96b6a9f549ff45c5fe520db68ee19268ae
edumunk <lacerda.snr@gmail.com>
2012-11-07 13:05:19 -0200

    Classes do package cliente implementadas.

--- Restaurant Le Marcelin/src/restaurant/model/cliente/Cliente.java
+++ Restaurant Le Marcelin/src/restaurant/model/cliente/Cliente.java
@@ -1,5 +1,51 @@
 package restaurant.model.cliente;
 
 public class Cliente {
+	
+	private String nome;
+	private String telefone;
+	private String endereco;
+	private String cpf;
+	
+	public Cliente() {
+		this.nome = "";
+		this.telefone = "";
+		this.endereco = "";
+		this.cpf = "";
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	
 
 }
--- Restaurant Le Marcelin/src/restaurant/model/cliente/Reserva.java
+++ Restaurant Le Marcelin/src/restaurant/model/cliente/Reserva.java
@@ -1,5 +1,33 @@
 package restaurant.model.cliente;
 
+import java.util.Date;
+
 public class Reserva {
+	
+	private Cliente cliente;
+	private Date data;
+	
+	public Reserva() {
+		cliente = new Cliente();
+		data = new Date();
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+	
+	
 
 }
