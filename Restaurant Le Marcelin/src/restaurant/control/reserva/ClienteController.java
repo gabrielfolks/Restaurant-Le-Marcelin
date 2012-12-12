515177e0614cfa1a7e10b9ceb43a93b1aef87ab9
Christopher Augusto <christopheeer@gmail.com>
2012-12-12 00:48:59 -0200

    concluído ClienteController e DAO

--- Restaurant Le Marcelin/src/restaurant/control/reserva/ClienteController.java
+++ Restaurant Le Marcelin/src/restaurant/control/reserva/ClienteController.java
@@ -1,5 +1,27 @@
 package restaurant.control.reserva;
 
+import restaurant.dao.DAOFactory;
+import restaurant.dao.interfaces.IClienteDAO;
+import restaurant.model.reserva.Cliente;
+
 public class ClienteController {
 
+	IClienteDAO clienteDAO = DAOFactory.createClienteDAO();
+	
+	public void adicionar(Cliente cliente) {
+		clienteDAO.adicionar(cliente);
+	}
+	
+	public void atualizar(Cliente cliente) {
+		clienteDAO.atualizar(cliente);
+	}
+	
+	public void remover(Cliente cliente) {
+		clienteDAO.remover(cliente);
+	}
+	
+	public void pesquisarPorCPF(String cpf) {
+		clienteDAO.procurarClientePorCPF(cpf);
+	}
+	
 }
--- Restaurant Le Marcelin/src/restaurant/dao/ClienteDAO.java
+++ Restaurant Le Marcelin/src/restaurant/dao/ClienteDAO.java
@@ -1,34 +1,133 @@
 package restaurant.dao;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import restaurant.dao.interfaces.IClienteDAO;
+import restaurant.dao.jdbc.Conexao;
 import restaurant.model.reserva.Cliente;
 
 public class ClienteDAO implements IClienteDAO {
 
+	private PreparedStatement prepStmt;
+	private CallableStatement callStmt;
+	private ResultSet resultSet;
+	private Connection connection;
+	
 	protected ClienteDAO(){}
 	
 	@Override
 	public void adicionar(Cliente cliente) {
-		// TODO Auto-generated method stub
-
+		connection = Conexao.getConexao();
+			
+		String sql = "{call insere_cliente (?,?,?,?)}";
+		
+		try {
+			callStmt = connection.prepareCall(sql);
+			
+			callStmt.setString(1, cliente.getNome());
+			callStmt.setString(2, cliente.getTelefone());
+			callStmt.setString(3, cliente.getEndereco());
+			callStmt.setString(4, cliente.getCpf());
+			
+			callStmt.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			fecharTudo();
+		}
 	}
 
 	@Override
 	public void atualizar(Cliente cliente) {
-		// TODO Auto-generated method stub
-
+		connection = Conexao.getConexao();
+		
+		String sql = "{call altera_cliente (?,?,?,?,?)}";
+		
+		try {
+			callStmt = connection.prepareCall(sql);
+			
+			callStmt.setInt(1, cliente.getId());
+			callStmt.setString(2, cliente.getNome());
+			callStmt.setString(3, cliente.getTelefone());
+			callStmt.setString(4, cliente.getEndereco());
+			callStmt.setString(5, cliente.getCpf());
+			
+			callStmt.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			fecharTudo();
+		}
 	}
 
 	@Override
 	public void remover(Cliente cliente) {
-		// TODO Auto-generated method stub
-
+		connection = Conexao.getConexao();
+		
+		String sql = "DELETE FROM Cliente WHERE idCliente = ?";
+		
+		try {
+			prepStmt = connection.prepareStatement(sql);
+			
+			prepStmt.setInt(1, cliente.getId());
+			
+			prepStmt.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			fecharTudo();
+		}
 	}
 
 	@Override
 	public Cliente procurarClientePorCPF(String cpf) {
-		// TODO Auto-generated method stub
-		return null;
+		connection = Conexao.getConexao();
+		Cliente cliente = new Cliente();
+				
+		String sql = "SELECT idCliente, nome, telefone, endereco, cpf FROM Cliente WHERE cpf = ?";
+		
+		try {
+			prepStmt = connection.prepareStatement(sql);
+			prepStmt.setString(1, cpf);
+			
+			resultSet = prepStmt.executeQuery();
+			resultSet.first();
+			
+			cliente.setId(resultSet.getInt(1));
+			cliente.setNome(resultSet.getString(2));
+			cliente.setTelefone(resultSet.getString(3));
+			cliente.setEndereco(resultSet.getString(4));
+			cliente.setCpf(resultSet.getString(5));
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			cliente = null;
+		} finally {
+			fecharTudo();
+		}
+		
+		return cliente;
 	}
 
+
+	private void fecharTudo() {
+		try {
+			if (connection != null)
+				connection.close();
+			if (resultSet != null)
+				resultSet.close();
+			if (prepStmt != null)
+			prepStmt.close();
+			if (callStmt != null)
+				callStmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	
+}
+	
 }
