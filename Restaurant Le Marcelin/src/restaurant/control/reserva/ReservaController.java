23035f728f47bbee6585689908a13535b0634333
Christopher Augusto <christopheeer@gmail.com>
2012-12-12 11:23:07 -0200

    reserva concluída

--- Restaurant Le Marcelin/src/restaurant/control/reserva/ReservaController.java
+++ Restaurant Le Marcelin/src/restaurant/control/reserva/ReservaController.java
@@ -1,5 +1,30 @@
 package restaurant.control.reserva;
 
+import java.util.Date;
+import java.util.Set;
+
+import restaurant.dao.DAOFactory;
+import restaurant.dao.interfaces.IReservaDAO;
+import restaurant.model.reserva.Reserva;
+
 public class ReservaController {
 
+	IReservaDAO reservaDAO = DAOFactory.createReservaDAO();
+	
+	public void adicionar(Reserva reserva) {
+		reservaDAO.adicionar(reserva);
+	}
+	
+	public void remover(Reserva reserva) {
+		reservaDAO.remover(reserva);
+	}
+	
+	public void atualizar(Reserva reserva) {
+		reservaDAO.atualizar(reserva);
+	}
+	
+	public Set<Reserva> procurarPorData(Date dataReserva) {
+		return reservaDAO.procurarPorData(dataReserva);
+	}
+	
 }
