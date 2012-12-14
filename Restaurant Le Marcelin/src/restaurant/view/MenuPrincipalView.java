f531334adc3feb8e460d0138e9a320823eb0145a
Christopher Augusto <christopheeer@gmail.com>
2012-12-14 17:35:57 -0200

    menu principal pronto

--- Restaurant Le Marcelin/src/restaurant/view/MenuPrincipalView.java
+++ Restaurant Le Marcelin/src/restaurant/view/MenuPrincipalView.java
@@ -2,7 +2,6 @@ package restaurant.view;
 
 import java.awt.CardLayout;
 import java.awt.Container;
-import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -10,10 +9,19 @@ import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import restaurant.view.estoque.FornecedorView;
+import restaurant.view.estoque.ProdutoView;
+import restaurant.view.franquia.CardapioView;
+import restaurant.view.franquia.FuncionarioView;
+import restaurant.view.franquia.LoginView;
 import restaurant.view.reserva.ClienteView;
+import restaurant.view.reserva.ReservaView;
+import restaurant.view.venda.CaixaView;
+import restaurant.view.venda.ComandaPedidoView;
+import restaurant.view.venda.DeliveryView;
 
 public class MenuPrincipalView extends JFrame implements ActionListener {
 
@@ -22,14 +30,24 @@ public class MenuPrincipalView extends JFrame implements ActionListener {
 	private JMenuItem mntmManterFuncionrios;
 	private JMenuItem mntmManterFornecedores;
 	private CardLayout layout;
+	private JMenuItem mntmReservas;
+	private JMenuItem mntmManterCardpio;
+	private JMenu mnSair;
+	private JMenuItem mntmEncerrarSesso;
+	private JMenuItem mntmFechar;
+	private JMenuItem mntmDelivery;
+	private JMenuItem mntmComanda;
+	private JMenuItem mntmManterProduto;
+	private JMenuItem mntmCaixa;
 
 	public MenuPrincipalView() {
 		initialize();
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
+	public static void main(String[] args) {
+		new MenuPrincipalView();
+	}
+	
 	private void initialize() {
 		this.setTitle("Administra\u00E7\u00E3o - Restaurant Le Marcelin");
 		this.setBounds(100, 100, 952, 488);
@@ -41,13 +59,31 @@ public class MenuPrincipalView extends JFrame implements ActionListener {
 		JMenu mnEstabelecimento = new JMenu("Estabelecimento");
 		menuBar.add(mnEstabelecimento);
 		
-		JMenuItem mntmReservas = new JMenuItem("Reserva de mesas");
+		mntmCaixa = new JMenuItem("Caixa");
+		mntmCaixa.addActionListener(this);
+		mnEstabelecimento.add(mntmCaixa);
+		
+		mntmComanda = new JMenuItem("Comanda");
+		mntmComanda.addActionListener(this);
+		mnEstabelecimento.add(mntmComanda);
+		
+		mntmDelivery = new JMenuItem("Delivery");
+		mntmDelivery.addActionListener(this);
+		mnEstabelecimento.add(mntmDelivery);
+		
+		mntmReservas = new JMenuItem("Reserva de mesas");
+		mntmReservas.addActionListener(this);
 		mnEstabelecimento.add(mntmReservas);
 		
 		JMenu mnProduto = new JMenu("Produto");
 		menuBar.add(mnProduto);
 		
-		JMenuItem mntmManterCardpio = new JMenuItem("Manter card\u00E1pio");
+		mntmManterProduto = new JMenuItem("Manter produto");
+		mntmManterProduto.addActionListener(this);
+		mnProduto.add(mntmManterProduto);
+		
+		mntmManterCardpio = new JMenuItem("Manter card\u00E1pio");
+		mntmManterCardpio.addActionListener(this);
 		mnProduto.add(mntmManterCardpio);
 		
 		JMenu mnRh = new JMenu("Recursos Humanos");
@@ -58,41 +94,78 @@ public class MenuPrincipalView extends JFrame implements ActionListener {
 		mntmManterClientes.addActionListener(this);
 		
 		mntmManterFuncionrios = new JMenuItem("Funcion\u00E1rios");
+		mntmManterFuncionrios.addActionListener(this);
 		mnRh.add(mntmManterFuncionrios);
 		
 		mntmManterFornecedores = new JMenuItem("Fornecedores");
 		mnRh.add(mntmManterFornecedores);
 		mntmManterFornecedores.addActionListener(this);
 		
-		JMenu mnLogout = new JMenu("Logout...");
-		menuBar.add(mnLogout);
+		mnSair = new JMenu("Sair");
+		menuBar.add(mnSair);
+		
+		mntmEncerrarSesso = new JMenuItem("Encerrar sess\u00E3o");
+		mntmEncerrarSesso.addActionListener(this);
+		mnSair.add(mntmEncerrarSesso);
+		
+		mntmFechar = new JMenuItem("Fechar");
+		mntmFechar.addActionListener(this);
+		mnSair.add(mntmFechar);
 		
 		painelPrincipal = new JPanel();
-		painelPrincipal.setLayout(layout);
-		
 		this.setContentPane(painelPrincipal);
+		painelPrincipal.setLayout(new CardLayout(0, 0));
 		setVisible(true);
+		setLocationRelativeTo(null);
 	}	
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == mntmManterClientes) {
-			Container painelCliente = new ClienteView().getContentPane();
+		if (e.getSource() == mntmEncerrarSesso) {
+			int showConfirmDialog = JOptionPane.showConfirmDialog(null, "Você deseja sair?");
 			
-			painelCliente.setSize(painelPrincipal.getSize());
-					
-			painelPrincipal.add("Cliente", painelCliente);
-			layout.show(painelPrincipal, "Cliente");
-			painelPrincipal.repaint();
-		} else if (e.getSource() == mntmManterFornecedores) {
-			Container painelFornecedores = new FornecedorView().getContentPane();
-			
-			painelFornecedores.setSize(painelPrincipal.getSize());
-			
-			painelPrincipal.add("Fornecedor", painelFornecedores);
-			layout.show(painelPrincipal, "Fornecedor");
-			painelPrincipal.repaint();
-		}
+			if (showConfirmDialog == 0) {
+				this.dispose();
+				LoginView lv = new LoginView();
+				lv.setVisible(true);
+			}
+		} 
 		
+		else if (e.getSource() == mntmFechar) {
+			int showConfirmDialog = JOptionPane.showConfirmDialog(null, "Você deseja sair?");
+			
+			if (showConfirmDialog == 0) 
+				System.exit(0);
+		
+		} 
+		
+		  else if (e.getSource() == mntmManterClientes) 
+			colocarPainelNaTela(new ClienteView().getContentPane());
+		  else if (e.getSource() == mntmManterFornecedores) 
+			colocarPainelNaTela(new FornecedorView().getContentPane());
+		  else if (e.getSource() == mntmManterFuncionrios) 
+			colocarPainelNaTela(new FuncionarioView().getContentPane());
+		  else if (e.getSource() == mntmComanda) 
+			  colocarPainelNaTela(new ComandaPedidoView().getContentPane());
+		  else if (e.getSource() == mntmCaixa)
+			  colocarPainelNaTela(new CaixaView().getContentPane());
+		  else if (e.getSource() == mntmDelivery)
+			  colocarPainelNaTela(new DeliveryView().getContentPane());
+		  else if (e.getSource() == mntmManterCardpio) 
+			  colocarPainelNaTela(new CardapioView().getContentPane());
+		  else if (e.getSource() == mntmReservas)
+			  colocarPainelNaTela(new ReservaView().getContentPane());
+		  else if (e.getSource() == mntmManterProduto)
+			  colocarPainelNaTela(new ProdutoView().getContentPane());
+		
+	}
+
+	private void colocarPainelNaTela(Container contentPane) {
+		painelPrincipal.removeAll();
+		
+		contentPane.setSize(painelPrincipal.getSize());
+		
+		painelPrincipal.add(contentPane);
+		painelPrincipal.repaint();
 	}
 }
